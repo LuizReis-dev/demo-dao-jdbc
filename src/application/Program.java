8eeb58b4b4acdf5ca1be3f8fed7da95b3ff2f408
LuizReis-dev <reisluiz05@gmail.com>
2021-08-04 19:06:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.00, obj);
 		System.out.println(obj);
-
+		System.out.println(seller);
+		
+		
 	}
 
 }
