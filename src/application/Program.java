a4553fd81623df300bd655f2e201ca7291177fad
LuizReis-dev <reisluiz05@gmail.com>
2021-08-04 18:53:44 -0300

    department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
