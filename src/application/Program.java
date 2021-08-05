0c637ce996657a4ff75f4e2672c4186db865ba06
LuizReis-dev <reisluiz05@gmail.com>
2021-08-05 09:54:25 -0300

    FindByID implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.00, obj);
-		System.out.println(obj);
-		System.out.println(seller);
-		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller =  sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,13 @@
 package model.dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	void update(Seller ob);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
 }
